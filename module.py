e91aad4df9d0f90fbb400974030257e1d66f35d4
ioan beilic <ioan@ioans-MacBook-Pro.local>
2026-02-24 00:24:11 +0100

    fix: remove leading indentation from module.py

--- module.py
+++ module.py
@@ -1,33 +1,33 @@
-    from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
-    MODULE_ID = 'surveys'
-    MODULE_NAME = _('Surveys')
-    MODULE_VERSION = '1.0.0'
-    MODULE_ICON = 'clipboard-outline'
-    MODULE_DESCRIPTION = _('Advanced surveys, questionnaires and response analytics')
-    MODULE_AUTHOR = 'ERPlora'
-    MODULE_CATEGORY = 'marketing'
+MODULE_ID = 'surveys'
+MODULE_NAME = _('Surveys')
+MODULE_VERSION = '1.0.0'
+MODULE_ICON = 'clipboard-outline'
+MODULE_DESCRIPTION = _('Advanced surveys, questionnaires and response analytics')
+MODULE_AUTHOR = 'ERPlora'
+MODULE_CATEGORY = 'marketing'
 
-    MENU = {
-        'label': _('Surveys'),
-        'icon': 'clipboard-outline',
-        'order': 62,
-    }
+MENU = {
+    'label': _('Surveys'),
+    'icon': 'clipboard-outline',
+    'order': 62,
+}
 
-    NAVIGATION = [
-        {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
+NAVIGATION = [
+    {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
 {'label': _('Surveys'), 'icon': 'clipboard-outline', 'id': 'surveys'},
 {'label': _('Responses'), 'icon': 'chatbox-ellipses-outline', 'id': 'responses'},
 {'label': _('Settings'), 'icon': 'settings-outline', 'id': 'settings'},
-    ]
+]
 
-    DEPENDENCIES = []
+DEPENDENCIES = []
 
-    PERMISSIONS = [
-        'surveys.view_survey',
+PERMISSIONS = [
+    'surveys.view_survey',
 'surveys.add_survey',
 'surveys.change_survey',
 'surveys.delete_survey',
 'surveys.view_responses',
 'surveys.manage_settings',
-    ]
+]
